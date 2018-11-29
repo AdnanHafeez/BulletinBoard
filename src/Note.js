caff73d87ed0fe1f115f4e12a668d7870ff03988
AdnanHafeez <adnanhr81@gmail.com>
2018-11-29 11:05:48 +1100

    Added code for adding, deleting and updating notes

--- src/Note.js
+++ src/Note.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import { FaTrash } from 'react-icons/fa';
 import { FaPencilAlt } from 'react-icons/fa';
-
+import {FaSave} from 'react-icons/fa';
 class Note extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      editing: false
+    };
     this.edit = this.edit.bind(this);
     this.remove = this.remove.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+    this.save = this.save.bind(this);
   }
+
   edit() {
-    alert('editing note');
+    this.setState({
+      editing:true
+    });
   }
   remove() {
-    alert('removing note');
+    this.props.onRemove(this.props.index);
   }
-  render() {
+  renderForm(){
     return (
       <div className="note">
-      <p> Learn React</p>
+        <form onSubmit={this.save}>
+          <textarea ref={input => {this._newText = input;} } />
+          <button onClick={this.save}><FaSave /></button>
+        </form>
+      </div>
+    )
+  }
+
+  save(e) {
+    e.preventDefault();
+    this.props.onChange(this._newText.value, this.props.index);
+    this.setState({
+      editing:false
+    })
+  }
+  renderDisplay() {
+    return (
+      <div className="note">
+      <p>{this.props.children} </p>
       <span>
       <button onClick={this.edit} id="edit"><FaPencilAlt /></button>
       <button onClick = {this.remove} id ="remove"><FaTrash /></button>
@@ -25,6 +51,12 @@ class Note extends Component {
       </div>
     )
   }
+
+  render() {
+
+    return this.state.editing ? this.renderForm() : this.renderDisplay();
+  }
+
 }
 
-export default Note
+export default Note;
